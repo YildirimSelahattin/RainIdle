657f1e917476b778c11eeb86f0722f2a17e82005
Selahattin Yıldırım <selahattin_yildirim@outlook.com>
2023-02-27 13:18:06 +0300

    Dancer Move

---
+++ Assets/Scripts/CircularMove.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircularMove : MonoBehaviour
+{
+    public float dancerSpeed = 5;
+    public float bodySpeed = 5;
+    public Transform totem;
+    public int Gap = 10;
+
+    // References
+    public GameObject BodyPrefab;
+
+    // Lists
+    public List<GameObject> BodyParts = new List<GameObject>();
+    private List<Vector3> PositionsHistory = new List<Vector3>();
+
+    // Update is called once per frame
+    void Update() {
+
+        // Move forward
+        transform.RotateAround(totem.position, Vector3.up, dancerSpeed * Time.deltaTime);
+
+        // Store position history
+        PositionsHistory.Insert(0, transform.position);
+
+        // Move body parts
+        int index = 0;
+        foreach (var body in BodyParts) {
+            Vector3 point = PositionsHistory[Mathf.Clamp(index * Gap, 0, PositionsHistory.Count - 1)];
+
+            // Move body towards the point along the snakes path
+            Vector3 moveDirection = point - body.transform.position;
+            body.transform.position += moveDirection * bodySpeed * Time.deltaTime;
+
+            // Rotate body towards the point along the snakes path
+            body.transform.LookAt(point);
+
+            index++;
+        }
+    }
+
+    public void GrowSnake() {
+        // Instantiate body instance and
+        // add it to the list
+        GameObject body = Instantiate(BodyPrefab);
+        BodyParts.Add(body);
+    }
+}
