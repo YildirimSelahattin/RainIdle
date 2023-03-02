9193da9744aeadfb96779acaddf9965c1cae5f8e
Selahattin Yıldırım <selahattin_yildirim@outlook.com>
2023-03-02 14:14:12 +0300

    .

---
+++ Assets/Scripts/RotateCircle.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCircle : MonoBehaviour
+{
+    public float planetSpeed = 2f;
+    void FixedUpdate()
+    {
+
+        transform.Rotate(Vector3.up * planetSpeed * Time.deltaTime);
+    }
+}
