7f8f68f0cafdb9a41ebfc3b59d3cb8a4dd4f5f3a
emirYarici <e-yarici@hotmail.com>
2023-03-06 16:34:07 +0300

    sa

--- Assets/Scripts/CropManager.cs
+++ Assets/Scripts/CropManager.cs
@@ -7,7 +7,8 @@ public class CropManager : MonoBehaviour
 {
     public float growTime = 10f;
     public int currentCircle;
-    Vector3 originalPos;
+
+
     void Start()
     {
         CropGrow();
@@ -15,15 +16,17 @@ public class CropManager : MonoBehaviour
 
     public void CropGrow()
     {
-        transform.DOLocalMove(new Vector3(0,2.5f, 0), 360/Mathf.Abs(GameManager.Instance.circleParentsList[currentCircle].GetComponent<RotateCircle>().planetSpeed));
+        transform.DOLocalMove(new Vector3(0, 2.5f, 0), (360 / Mathf.Abs(GameManager.Instance.circleParentsList[currentCircle].GetComponent<RotateCircle>().planetSpeed)));
+
     }
-    
-    private void OnTriggerEnter(Collider other)
+
+    private void OnTriggerExit(Collider other)
     {
+
         if (other.gameObject.tag == "Cutter1")
         {
-            transform.DOLocalMove(new Vector3(0,-10f, 0), 0.1f);
-            CropGrow();
+            Debug.Log("sa");
+            transform.DOLocalMoveY(1f, 0.2f).OnComplete(() => CropGrow());
         }
     }
 }
